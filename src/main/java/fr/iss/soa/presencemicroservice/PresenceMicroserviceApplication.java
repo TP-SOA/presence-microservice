6e68e17e93a23f1335ca86d438b738fad68d6d7e
bcadot <55708764+bcadot@users.noreply.github.com>
2021-12-10 09:24:47 +0100

    chore: generate spring boot project

---
+++ src/main/java/fr/iss/soa/presencemicroservice/PresenceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fr.iss.soa.presencemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresenceMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/iss/soa/presencemicroservice/PresenceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iss.soa.presencemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
