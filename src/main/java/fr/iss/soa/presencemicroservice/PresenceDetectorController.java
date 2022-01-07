0b9b17cbe39f19441e8424a40dcb225c71149754
bcadot <55708764+bcadot@users.noreply.github.com>
2022-01-07 10:21:46 +0100

    feat: add simulate presence in constructor

--- src/main/java/fr/iss/soa/presencemicroservice/PresenceDetectorController.java
+++ src/main/java/fr/iss/soa/presencemicroservice/PresenceDetectorController.java
@@ -12,12 +12,40 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @RestController
 public class PresenceDetectorController {
     private ArrayList<PresenceDetector> presenceDetectors = new ArrayList<>();
 
+    private String ip = "192.168.43.202:8080";
+
     public PresenceDetectorController() throws URISyntaxException {
+        //Simulate a presence in room 11 every 5 seconds
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    sendPresenceDetected(11);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 5*1000);
+        //Simulate a presence in room 213 every 30 seconds
+        Timer t1 = new Timer();
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    sendPresenceDetected(213);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 30*1000);
     }
 
     @GetMapping("/detectors")
@@ -42,13 +70,13 @@ public class PresenceDetectorController {
     }
 
     public void sendPresenceDetected(int id) throws URISyntaxException {
-        URI centralService = new URI("http://localhost:8080/presence-event/"+id);
+        System.out.println("Trying to execute POST request.");
+        URI centralService = new URI("http://" + ip + "/presence-event/" + id);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.postForEntity(centralService, null, String.class);
 
-        if (response.getStatusCode() != HttpStatus.OK) {
+        if (response.getStatusCode() != HttpStatus.OK)
             System.err.println("Oops, " + id + " does not exist.");
-        }
     }
 
 }
